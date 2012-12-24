c47c5d901d12ef76071a09db433e67d6792e0fd1
Ted Held <tedheld@gmail.com>
2012-12-24 14:27:31 -0600

    bootstrapped some forms

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,10 +1,11 @@
 class Person < ActiveRecord::Base
   attr_accessible :address, :city, :firstname, :lastname, :state
   has_many :sales
+  validates :firstname, :lastname, :address, :city, :state, :presence => true
 
   def self.search(search)
     if search
-      find(:all, :conditions => ['lastname LIKE ?', "%#{search}%"])
+      find(:all, :conditions => ['lastname LIKE ? OR firstname LIKE ?', "%#{search}%", "%#{search}%"])
     else
       find(:all, :order => "lastname")
     end
