646a50afbb1891ec7960e586f1d956ba599fdb89
Ted Held <tedheld@gmail.com>
2013-04-26 12:59:54 -0500

    Silenced deprecation warnings in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Silence deprecation warnings - added on account of Heroku bug
+ActiveSupport::Deprecation.silenced = true
+
 # Initialize the rails application
 PipestoneApp::Application.initialize!
